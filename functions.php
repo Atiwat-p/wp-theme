42d0ff3eb23fe687bc6a2d941d564001cf60622b
Atiwat-p <59089524+Atiwat-p@users.noreply.github.com>
2020-02-01 20:56:26 +0800

    6 commit
    setting the coding part for each php files for correct position

--- functions.php
+++ functions.php
@@ -2,8 +2,15 @@
 
 function wp_theme() {
 
-    wp_enqueue_style('style', get_stylesheet_uri());
+  wp_enqueue_style('style', get_stylesheet_uri());
 
 }
 
 add_action('wp_enqueue_scripts', 'wp_theme');
+
+
+// Navigation Menus
+register_nav_menus(array(
+    'footer' => __( 'Footer Menu' ),
+
+));
--- header.php
+++ header.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo('charset'); ?>">
-        <meta name="viewport" content="width=device-width">
-        <title><?php bloginfo('name'); ?></title>
-        <?php wp_head(); ?>
-    </head>
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+  </head>
 
 <body <?php body_class(); ?>>
 
-    <div class="container">
+  <div class="container">
 
-        <!-- site-header -->
-        <header class="site-header">
-            <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-            <h5><?php bloginfo('description'); ?></h5>
+    <!-- site-header -->
+    <header class="site-header">
+      <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+      <h5><?php bloginfo('description'); ?></h5>
 
-        </header><!-- /site-header -->
+    </header><!-- /site-header -->
 
-            <form method="get" action="<?php print site_url(); ?>">
-              <input type-"text" name="s" value="<?php if(isset($_GET['s'])){print $_GET['s'];}?>">
-              <input type="submit" value="Search for our site ....">
-            </form>
+        <form method="get" action="<?php print site_url(); ?>">
+          <input type-"text" name="s" value="<?php if(isset($_GET['s'])){print $_GET['s'];}?>">
+          <input type="submit" value="Search for our site ....">
+        </form>
